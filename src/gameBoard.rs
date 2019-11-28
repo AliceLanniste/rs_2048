36a188c0077289c021fe0525e8743070d9718033
Alice <1399789151@qq.com>
2019-11-28 16:52:37 +0800

    initially commit

---
+++ src/gameBoard.rs
@@ -0,0 +1,79 @@
+use rand::thread_rng;
+
+#[derive(Debug)]
+pub struct GameBoard {
+   
+   board: [[Option<i32>;4];4],
+}
+
+impl  GameBoard {
+    fn new() -> Self {
+       
+       Self {
+         board: [[None;4];4],
+       }
+    }
+//    +------+------+------+------+
+//    | (0,0)|      |      | (0,3)|
+//    +------+------+------+------+ 
+//    | (1,0)|      |      | (1,3)|
+//    +------+------+------+------+ 
+//    | (2,0)|      |      | (2,3)|
+//    +------+------+------+------+ 
+//    | (3,0)|      |      | (3,3)|
+//    +------+------+------+------+ 
+
+
+   pub fn display()  {
+        let line =  "  +------+------+------+------+";
+        let delimiter = "  | ";
+        for _i in 0..=3 {
+            println!("{}",line);
+            
+            for _j in 0..=3 {
+                print!("{:width$}",delimiter,width=7);
+            }
+
+            println!("{}",delimiter);
+        }
+
+        println!("{}",line);
+    }
+
+  //随机选位置[x][y],然后随机选2或4，2出现的概率要大于4
+    fn fill_in(&mut self)  {
+       
+        unimplemented!();
+    }
+    
+    fn position(&self) -> Vec<(usize,usize)> {
+        let mut empty_position = Vec::new();
+       for i in 0..=3 {
+           for j in 0..=3 {
+               if self.board[i][j] ==None {
+                   empty_position.push((i,j));
+               }
+           }
+       }
+        
+        empty_position
+    }
+
+    //产生2和4
+    fn rand_number() -> i32 {
+        let choices = [2,2,2,4];
+        
+        unimplemented!();
+    }
+
+    fn testAdd(&self,x:usize,y:usize, value: i32) ->bool{
+        if x >3 || y > 3 {
+            return false;
+        }
+
+        Some(value) == self.board[x][y] 
+       
+    }
+
+  
+}
